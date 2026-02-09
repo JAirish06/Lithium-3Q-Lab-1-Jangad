5446134b24b32872b0f1d4cc7c2194ca00e7ecc7
JAirish06 <jamjangad29@src.pshs.edu.ph>
2026-02-09 22:02:48 +0800

    Refactor kinetic energy calculation in script.js
    Updated the calculation of relativistic kinetic energy to use a more accurate formula and changed the speed of light representation.

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const c = (2.9978)*(10**8);
+const c = 299780000;
 
 function calculateTheEquation(){
    let velocity = parseFloat(document.getElementById("input-velocity").value);   
    let mass = parseFloat(document.getElementById("input-mass").value);     
-   let k = (mass)*(c**2)/(Math.sqrt(1/1-(velocity**2)/c**2));
+  let k = mass * c *c *(Math.sqrt(1/(1-(velocity*velocity)/(c*c))))-mass * c*c;
 
-
-   let output = `The Relativistic Kinetic Energy ${k}.`;
+   let output = `The Relativistic Kinetic Energy ${k} Joules.`;
    document.getElementById("result-relativistic").innerText = output;
 }
-
 
