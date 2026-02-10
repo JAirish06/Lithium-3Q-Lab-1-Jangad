fd18f9e3be43f91427e3c30bde383ee348e14f49
JAirish06 <jamjangad29@src.pshs.edu.ph>
2026-02-10 08:34:13 +0800

    Refactor calculateTheEquation for kinetic energy calculation

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const c = 299780000;
+const c = 2.9978e8;
 
 function calculateTheEquation(){
    let velocity = parseFloat(document.getElementById("input-velocity").value);   
    let mass = parseFloat(document.getElementById("input-mass").value);     
-  let k = mass * c *c *(Math.sqrt(1/(1-(velocity*velocity)/(c*c))))-mass * c*c;
+  let k = (mass*c**2)/Math.sqrt(1/(1-velocity**2/(c**2)));
 
    let output = `The Relativistic Kinetic Energy ${k} Joules.`;
    document.getElementById("result-relativistic").innerText = output;
 }
-
